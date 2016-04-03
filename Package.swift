43080122f99aec212b66d162e13eae5b911936fe
Scott Byrns <contact@scottbyrns.com>
2016-04-03 17:15:23 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 let package = Package(
     name: "HTTP",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/MediaType.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/scottbyrns/MediaType.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/SwiftX/S4.git", majorVersion: 0, minor: 1),
