eb52dd519592834924b7404f43cb527ed6da3a8e
Scott Byrns <contact@scottbyrns.com>
2016-04-03 17:24:06 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -30,6 +30,6 @@ let package = Package(
         .Package(url: "https://github.com/scottbyrns/MediaType.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/scottbyrns/URI.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/scottbyrns/S4.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/scottbyrns/S4.git", majorVersion: 0, minor: 2),
     ]
 )
